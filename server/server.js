9088d3ce78af4af3ec8dc93e69c26bf4068d5d16
Vito Senic <67893442+pegi4@users.noreply.github.com>
2024-10-27 22:31:58 +0100

    x

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 require('dotenv').config();
-app.use(cors({ origin: '*' }));
+const cors = require('cors');
 
 const app = express();
 app.use(cors()); 
